74369163943fea8d7ff71146aa92c396ed6f58aa
MethMicha <michael.k.lindner@t-online.de>
2022-10-21 11:17:35 +0200

    enemy spawn fix

--- main.py
+++ main.py
@@ -11,19 +11,31 @@ fps = 60
 tower_x = 0
 tower_y = 0
 tower_level = 1
-enemy_x = 30
-enemy_y = 30
-enemy_life = 0
-enemy_speed = 1
+
+
+# https://stackoverflow.com/questions/63523461/spawn-multiple-enemies-pygame
+
+enemy_img = pygame.image.load("graphics/monster_lvl1.png")
+enemy = []
+enemyX = []
+enemy_spawnX = 300
+enemyY = []
+enemy_spawnY = 300
+enemy_num = 0
+spawn_stop = False
 
 maus_pos = pygame.mouse.get_pos()
 maus_klick = pygame.mouse.get_pressed()
 
 pixel_font = pygame.font.Font("fonts/PixeloidSans.ttf", 30)
 
-case_val = 1
-enemy_count = 1
-
+case_val1 = 1
+case_val2 = 1
+case_val3 = 1
+case_val4 = 1
+case_val5 = 1
+len_array = 0
+count = 0
 
 background = pygame.image.load("graphics/tower_lvl11.png").convert_alpha()
 
@@ -33,46 +45,6 @@ class tower:
         self.tower_y = tower_y
         self.tower_level = tower_level
 
-class enemy:
-    def __init__(self, enemy_type, enemy_x, enemy_y, enemy_life, enemy_speed, case_val):
-        self.enemy_x = enemy_x
-        self.enemy_y = enemy_y
-        self.enemy_life = enemy_life
-        self.enemy_speed = enemy_speed
-        self.enemy_type = enemy_type
-        self.case_val = case_val
-
-    def blit(self):
-
-        enemy_lvl1 = pygame.image.load("graphics/monster_lvl1.png").convert_alpha()
-
-        if self.enemy_type == "enemy_l1":
-            screen.blit(enemy_lvl1, (self.enemy_x, self.enemy_y))
-            print(str(self.enemy_y) + " <-Y   X-> " + str(self.enemy_x))
-
-
-        match self.case_val:
-            case 1:
-                self.enemy_y += self.enemy_speed
-                print("Case 1")
-                if self.enemy_y >= 386:
-                    self.case_val += 1
-            case 2:
-                self.enemy_x += self.enemy_speed
-                print("Case 2")
-                if self.enemy_x >= 800:
-                    self.case_val += 1
-            case 3:
-                self.enemy_y += self.enemy_speed
-                print("Case 3")
-
-
-    def collide_wall(self):
-        lifes = 10
-        if enemy_x > 1920 or enemy_y > 1080:
-            lifes -= 1
-            print("lifes " + str(lifes) + " von 10 übrig")
-            return True
 
 
 def textObjekt(text, pixel_font):
@@ -108,29 +80,36 @@ def userinterface():
     pygame.draw.rect(screen, "Black", (1520, 0, 400, 1080))
 
 
-enemy1 = enemy("enemy_l1", 215, 0, 0, 1, case_val)
-
 runtime = True
 while runtime:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runtime = False
 
+
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_SPACE]:
         runtime = False
     if pressed[pygame.K_w]:
-        while pressed[pygame.k_w]:
-            enemy_count += 1
-            enemy[enemy_count]
+        enemy_num += 1
 
 
+
+
+    for i in range(enemy_num):
+        enemy.append(enemy_img)
+        enemyX.append(215)
+        enemyY.append(0)
+
+    for i in range(enemy_num):
+        enemyY[i] += 4
 
 
     screen.blit(background, (0,0))
+    for i in range(enemy_num):
+        screen.blit(enemy[i], (enemyX[i], enemyY[i]))
+
     userinterface()
-    enemy1.blit()
-    enemy1.collide_wall()
     pygame.display.flip()
 
     clock.tick(fps)
