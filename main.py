0a0e68c9037fce74ef235091f0905fb729772f6b
MethMicha <michael.k.lindner@t-online.de>
2022-10-23 21:40:54 +0200

    kein plan, setting menü

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 import sys
 
+
+
 pygame.init()
 
-global maus_aktiv
+
 global option
 global setting_state
 global set_decider
+
 
 option = "Home"
 
@@ -27,6 +30,8 @@ enemy_spawn_sec = 1     # -> Sekunden
 
 user_input = ""
 set_decider = ""
+case_val = 0
+case_val2 = 0
 
 # https://stackoverflow.com/questions/63523461/spawn-multiple-enemies-pygame
 
@@ -39,6 +44,7 @@ enemy_spawnY = 300
 global enemy_num
 enemy_num = 0
 spawn_stop = False
+slider_bool = True
 
 maus_pos = pygame.mouse.get_pos()
 maus_klick = pygame.mouse.get_pressed()
@@ -96,12 +102,16 @@ def setting_button(set_state, but_x, but_y, but_laenge, but_hoehe, but_color_0, 
     if maus_pos[0] > but_x and maus_pos[0] < but_x + but_laenge and maus_pos[1] > but_y and maus_pos[1] < but_y+but_hoehe:
         pygame.draw.rect(screen, but_color_1, (but_x, but_y, but_laenge, but_hoehe))
         if maus_klick[0] == 1 and maus_aktiv == False:
-            maus_aktiv = True
-            set_decider = set_state
+            if set_state == "+":
+                set_decider = "+"
+            if set_state == "-":
+                set_decider = "-"
         if maus_klick[0] == 0:
             maus_aktiv = False
+            set_decider = ""
     else:
         pygame.draw.rect(screen, but_color_0, (but_x, but_y, but_laenge, but_hoehe))
+
     textGrund, textkasten = textObjekt(set_state, but_font)
     textkasten.center = ((but_x+(but_laenge/2)),(but_y+(but_hoehe/2)))
     screen.blit(textGrund, textkasten)
@@ -124,19 +134,11 @@ def userinterface():
     button("Exit", 1790, 980, 80, 45, "Green", "Red", pixel_font15)
 
 def slide_setting(slide_text, min_val, max_val, slide_x, slide_y):
-    global set_decider
-    draw_text(str(slide_text), pixel_font30, (0,0,0), screen, slide_x, slide_y)
 
-    rec_x = slide_x + 350
-    rec_y = slide_y
-    button("-", slide_x + 240, slide_y, 40, 40, (68, 212, 219), "Grey", sys_font30)
-    if set_decider == "-":
-        rec_x -= 35
-    elif set_decider == "+":
-        rec_x += 35
+    draw_text(str(slide_text), pixel_font30, (0,0,0), screen, slide_x, slide_y)
+    setting_button("-", slide_x + 240, slide_y, 40, 40, (68, 212, 219), "Grey", sys_font30)
     draw_text(str(min_val), pixel_font30, (0, 0, 0), screen, slide_x + 300,slide_y)
-    pygame.draw.rect(screen, (0,0,0), (slide_x + 350, rec_y+10, 320, 15))
-    pygame.draw.rect(screen, (0,0,0), (rec_x, rec_y, 35, 35))
+    pygame.draw.rect(screen, (0, 0, 0), (slide_x + 350, rec_y+10, 320, 15))
     draw_text(str(max_val), pixel_font30, (0, 0, 0), screen, slide_x + 700,slide_y)
     setting_button("+", slide_x + 750, slide_y, 40, 40, (68, 212, 219), "Grey", sys_font30)
 
@@ -153,7 +155,6 @@ def settings():
     screen.blit(home_background, (0, 0))
     pygame.draw.rect(screen, (48, 79, 84), (510, 0, 900, 1080))
     draw_text("Settings", pixel_font60, (0, 0, 0), screen, 860, 0)
-
     button("Home", 755, 980, 110, 45, (68, 212, 219), "Green", pixel_font30)
     button("Resume", 1055, 980, 140, 45, (68, 212, 219), "Green", pixel_font30)
 
@@ -165,13 +166,15 @@ def debug():
     print(str(ticker_sec) + "<-Ticker Sec  - EnNum -> " + str(enemy_num))
     print(maus_pos[0], maus_pos[1])
 
-
+rec_x = 930
+rec_y = 300
 
 runtime = True
 while runtime:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runtime = False
+
 
 #region #Deffinitionen updaten
     maus_pos = pygame.mouse.get_pos()
@@ -204,7 +207,6 @@ while runtime:
 #endregion
 
 
-
     #draw_text("TestTestTest", sys_font15, "Red", screen, 100, 100)
     if option == "Home":
         homescreen()
@@ -220,11 +222,62 @@ while runtime:
         userinterface()
         for i in range(enemy_num):
             screen.blit(enemy[i], (enemyX[i], enemyY[i]))
+
     elif option == "Settings":
         settings()
         slide_setting("Test", 0, 10, 580, 300)
+        if maus_klick[0] == 1 and maus_aktiv == False:
+            maus_aktiv = True
+            if set_decider == "+":
+                case_val +=1
+                if case_val >= 9:
+                    case_val = 9
+            elif set_decider == "-":
+                case_val -= 1
+                if case_val <= 0:
+                    case_val = 0
+        if maus_klick[0] == 0:
+            maus_aktiv = False
 
-    print(set_decider)
+        match case_val:
+            case 0:
+                pygame.draw.rect(screen, "Grey", (900, 300, 35, 35))
+            case 1:
+                pygame.draw.rect(screen, "Grey", (935, 300, 35, 35))
+            case 2:
+                pygame.draw.rect(screen, "Grey", (970, 300, 35, 35))
+            case 3:
+                pygame.draw.rect(screen, "Grey", (1005, 300, 35, 35))
+            case 4:
+                pygame.draw.rect(screen, "Grey", (1040, 300, 35, 35))
+            case 5:
+                pygame.draw.rect(screen, "Grey", (1075, 300, 35, 35))
+            case 6:
+                pygame.draw.rect(screen, "Grey", (1110, 300, 35, 35))
+            case 7:
+                pygame.draw.rect(screen, "Grey", (1145, 300, 35, 35))
+            case 8:
+                pygame.draw.rect(screen, "Grey", (1180, 300, 35, 35))
+            case 9:
+                pygame.draw.rect(screen, "Grey", (1215, 300, 35, 35))
+
+        slide_setting("Musik", "OFF", "ON", 580, 600)
+        if maus_klick[0] == 1 and maus_aktiv == False:
+            print(maus_aktiv)
+            maus_aktiv = True
+            if set_decider == "+":
+                case_val2 += 1
+            elif set_decider == "-":
+                case_val2 -= 1
+        if maus_klick[0] == 0:
+            maus_aktiv = False
+        match case_val2:
+            case 0:
+                pygame.draw.rect(screen, "Grey", (900, 600, 35, 35))
+            case 1:
+                pygame.draw.rect(screen, "Grey", (935, 600, 35, 35))
+
+
 
     show_fps()
     pygame.display.flip()
